1ab9b7027ae22a76fb810f6cd7cab12d3f3cba87
Nitish <nitishingde@protonmail.com>
2023-04-30 03:16:26 -0600

    Update cmake and run script

--- run.sh
+++ run.sh
@@ -40,5 +40,36 @@ if [ -f "cuda_lbm_lid_cavity_2048x2048.pydat" ]; then
   python3 display.py cuda_lbm_lid_cavity_2048x2048.pydat
 fi
 
+echo ""
+echo "Running Lid-driven Cavity cuda code using Shared Memory:"
+
+./cmake-build-release/cudaSmLidDrivenCavity-128
+if [ -f "cuda_sm_lbm_lid_cavity_128x128.pydat" ]; then
+  python3 display.py cuda_sm_lbm_lid_cavity_128x128.pydat
+fi
+
+./cmake-build-release/cudaSmLidDrivenCavity-256
+if [ -f "cuda_sm_lbm_lid_cavity_256x256.pydat" ]; then
+  python3 display.py cuda_sm_lbm_lid_cavity_256x256.pydat
+fi
+
+./cmake-build-release/cudaSmLidDrivenCavity-512
+if [ -f "cuda_sm_lbm_lid_cavity_512x512.pydat" ]; then
+  python3 display.py cuda_sm_lbm_lid_cavity_512x512.pydat
+fi
+
+./cmake-build-release/cudaSmLidDrivenCavity-1024
+if [ -f "cuda_sm_lbm_lid_cavity_1024x1024.pydat" ]; then
+  python3 display.py cuda_sm_lbm_lid_cavity_1024x1024.pydat
+fi
+
+./cmake-build-release/cudaSmLidDrivenCavity-2048
+if [ -f "cuda_sm_lbm_lid_cavity_2048x2048.pydat" ]; then
+  python3 display.py cuda_sm_lbm_lid_cavity_2048x2048.pydat
+fi
+
+echo ""
+echo "Running Lid-driven Cavity serial code:"
+
 ./cmake-build-release/serialLidDrivenCavity
-python3 display.py ./lbm_lid_cavity_0.pydat
+python3 display.py serial_lbm_lid_cavity.pydat
